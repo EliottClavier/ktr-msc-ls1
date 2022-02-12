b45755909ff019b74414bd60ba3c50a2911a1ab5
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-02-12 13:48:17 +0100

    Add bootstrap + create form Create Profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const REGISTER_FIELDS = [
+  { type: "text", name: "name", label: "Name", required: true },
+  { type: "text", name: "companyName", label: "Company name", required: false },
+  { type: "email", name: "mail", label: "Email address", required: false },
+  { type: "tel", name: "phoneNumber", label: "Telephone number", required: false, pattern: "[0-9]{10}" },
+]
+
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Register' });
+  res.render('register', { title: 'Register', fields: REGISTER_FIELDS });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  res.end()
 });
 
 module.exports = router;
